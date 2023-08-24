0f8751b18422c602397f303c02f8c2e0bf0eb86e
lihaifei <mecuryhai@gmail.com>
2023-08-25 01:41:49 +0800

    增加数打印与初始化能力

--- src/main/java/org/example/bo/TreeNode.java
+++ src/main/java/org/example/bo/TreeNode.java
@@ -17,4 +17,31 @@ public class TreeNode {
         this.left = left;
         this.right = right;
     }
+
+    public static TreeNode initByArr(Integer[] arr) {
+
+        TreeNode[] nodeArr = new TreeNode[arr.length];
+        nodeArr[0] = new TreeNode(arr[0]);
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                nodeArr[i] = null;
+                continue;
+            } else {
+                nodeArr[i] = new TreeNode(arr[i]);
+            }
+
+            TreeNode parent = nodeArr[(i - 1) / 2];
+            if (parent == null) {
+                continue;
+            }
+            if ((i - 1) % 2 == 0) {
+                parent.left = nodeArr[i];
+            } else {
+                parent.right = nodeArr[i];
+            }
+        }
+
+        return nodeArr[0];
+    }
 }
