618bcc4875c834fc09464401da4c22ba6240519d
lihaifei04 <lihaifei04@meituan.com>
2024-03-08 16:37:51 +0800

    feat(dp): 经典01背包问题

---
+++ algorithm4/src/main/java/org/example/dp/dp_01/DP_02_Package.java
@@ -0,0 +1,6 @@
+package org.example.dp.dp_01;/**
+ * @Description: 
+ * @Author: lihaifei04
+ * @Date: 2024/3/8 16:37
+ */public class DP_02_Package {
+}
--- algorithm4/src/main/java/org/example/dp/dp_01/dp_01_package.java
+++ algorithm4/src/main/java/org/example/dp/dp_01/dp_01_package.java
@@ -1,5 +1,7 @@
 package org.example.dp.dp_01;
 
+import org.junit.Test;
+
 /**
  * @Description:
  * @Author: lihaifei04
@@ -7,4 +9,29 @@ package org.example.dp.dp_01;
  */
 public class dp_01_package {
 
+    @Test
+    public void test() {
+
+    }
+
+    /**
+     * @param w 重量
+     * @param v 价值
+     * @param N 最大数量
+     * @param W 最大重量
+     * @return
+     */
+    int dp(int[] w, int[] v, int N, int W) {
+        int[][] dp = new int[N + 1][W + 1];
+        for (int n = 1; n < N + 1; n++) {                   // 遍历物品
+            for (int weight = 1; weight <= W; weight++) {   // 背包容量有多大就还要计算多少次
+                if (weight < w[n]) {
+                    dp[n][weight] = dp[n - 1][weight];
+                } else {
+                    dp[n][weight] = Math.max(dp[n - 1][weight - w[n]] + v[n], dp[n - 1][weight]);
+                }
+            }
+        }
+        return dp[N][W];
+    }
 }
